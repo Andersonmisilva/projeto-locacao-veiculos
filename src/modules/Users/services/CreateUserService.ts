541c5f7a891b2420be7f6698440e14c4201444ba
Andersonmisilva <123589169+Andersonmisilva@users.noreply.github.com>
2023-04-20 01:11:43 -0300

    Senhas Criptografadas

--- src/modules/Users/services/CreateUserService.ts
+++ src/modules/Users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/entities/repositories/UsersRepository';
+import { hash } from 'bcryptjs';
 
 interface IRequest {
   name: string;
@@ -18,10 +19,12 @@ class CreateUserService {
       throw new AppError('Email address already used.');
     }
 
+    const hashPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashPassword,
     });
 
     await usersRepository.save(user);
